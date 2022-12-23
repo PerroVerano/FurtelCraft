bdf5f2a3da06f23bc630f6836a65835042545514
V2x5aHV2 <xf15765227015@outlook.com>
2022-12-23 19:31:44 +0800

    [1023]data update&add

---
+++ src/main/java/com/vtwo/furtelcraft/furtelcraft/mixin/TitleScreenMixin.java
@@ -0,0 +1,9 @@
+package com.vtwo.furtelcraft.furtelcraft.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+
+}
